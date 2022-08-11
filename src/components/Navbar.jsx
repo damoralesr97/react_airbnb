4518a4f8fb1f0ea00fcec276a22de31a6032dbe1
desarrollo <desarrollo@192.168.0.19>
2022-08-11 09:30:00 -0500

    project done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <img className="nav--logo" src="../airbnb-logo.png"></img>
+    </nav>
+  );
+}
+
+export default Navbar;
